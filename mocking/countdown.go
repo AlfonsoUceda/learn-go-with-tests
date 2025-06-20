3a11609c80acffb9542c765fd560630b1f125563
Alfonso Uceda <alfonso@hubbado.com>
2025-06-20 10:13:29 +0200

    Mocking: countdown v2

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -11,24 +11,28 @@ const finalWord = "Go!"
 const countdownStart = 3
 
 type Sleeper interface {
-    Sleep()
+	Sleep()
 }
 
-type DefaultSleeper struct{}
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
 
-func (d *DefaultSleeper) Sleep() {
-    time.Sleep(1 * time.Second)
+func (c *ConfigurableSleeper) Sleep() {
+    c.sleep(c.duration)
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
-    for i := countdownStart; i > 0; i-- {
-        fmt.Fprintln(out, i)
-        sleeper.Sleep()
-    }
-    fmt.Fprint(out, finalWord)
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-    sleeper := &DefaultSleeper{}
-    Countdown(os.Stdout, sleeper)
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
 }
